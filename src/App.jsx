491eb2d83af41161ba30700fc9f70835e74ac722
alpha951 <20uec068@lnmiit.ac.in>
2023-11-08 01:27:47 +0530

    ADD navbar initial design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import NavbarComponent from "./components/navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className='card'>Hi there</div>
+      <NavbarComponent />
     </>
   );
 }
