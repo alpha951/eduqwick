0dda499954e86e89352d6b519b6d76ebf1f26a84
alpha951 <20uec068@lnmiit.ac.in>
2023-11-11 14:47:54 +0530

    Update navbar dynamic dropdown and style

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,75 +1,163 @@
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-
+import { useState, useEffect } from "react";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import "./Navbar.css";
 
 function NavbarComponent() {
+  const [collegesData, setCollegesData] = useState([]);
+  const [coursesData, setCoursesData] = useState([]);
+  const [examsData, setExamsData] = useState([]);
+
+  useEffect(() => {
+    // Simulate fetching data from a JSON file
+    // Replace this with your actual data fetching logic when your backend is ready
+    fetch("../../../data/colleges.json")
+      .then((response) => response.json())
+      .then((data) => setCollegesData(data))
+      .catch((error) => console.error("Error fetching colleges data:", error));
+
+    fetch("../../../data/courses.json")
+      .then((response) => response.json())
+      .then((data) => setCoursesData(data))
+      .catch((error) => console.error("Error fetching colleges data:", error));
+
+    fetch("../../../data/exams.json")
+      .then((response) => response.json())
+      .then((data) => setExamsData(data))
+      .catch((error) => console.error("Error fetching colleges data:", error));
+  }, []);
+
+  const [showCollegesDropdown, setShowCollegesDropdown] = useState(false);
+  const [showCoursesDropdown, setShowCoursesDropdown] = useState(false);
+  const [showExamsDropdown, setShowExamsDropdown] = useState(false);
+
+  const handleCollegesMouseEnter = () => {
+    setShowCollegesDropdown(true);
+  };
+
+  const handleCollegesMouseLeave = () => {
+    setShowCollegesDropdown(false);
+  };
+
+  const handleCoursesMouseEnter = () => {
+    setShowCoursesDropdown(true);
+  };
+
+  const handleCoursesMouseLeave = () => {
+    setShowCoursesDropdown(false);
+  };
+
+  const handleExamsMouseEnter = () => {
+    setShowExamsDropdown(true);
+  };
+
+  const handleExamsMouseLeave = () => {
+    setShowExamsDropdown(false);
+  };
+
   return (
-    <Navbar expand='lg' className='bg-body-tertiary'>
-      <Container fluid>
-        <Navbar.Brand href='#'>
-          <img src='../../ui-idea/brand-logo.png' className='brand-logo'></img>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls='navbarScroll' />
-        <Navbar.Collapse id='navbarScroll'>
-          <Nav
-            className='me-auto my-2 my-lg-0'
-            style={{ maxHeight: "500px" }}
-            navbarScroll>
-            <NavDropdown title='Colleges' id='navbarScrollingDropdown'>
-              <NavDropdown.Item href='#action3'>Action</NavDropdown.Item>
-              <NavDropdown.Item href='#action4'>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href='#action5'>
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <NavDropdown title='Courses' id='navbarScrollingDropdown'>
-              <NavDropdown.Item href='#action3'>Action</NavDropdown.Item>
-              <NavDropdown.Item href='#action4'>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href='#action5'>
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <NavDropdown title='Exams' id='navbarScrollingDropdown'>
-              <NavDropdown.Item href='#action3'>Action</NavDropdown.Item>
-              <NavDropdown.Item href='#action4'>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href='#action5'>
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            
-            <Nav.Link href='#action2'>Study Abroad</Nav.Link>
-            <Nav.Link href='#action2'>Get Counselling</Nav.Link>
-            <Nav.Link href='#action2'>Latest News</Nav.Link>
-          </Nav>
-
-          <Form className='d-flex'>
-            <Form.Control
-              type='search'
-              placeholder='Search'
-              className='me-2'
-              aria-label='Search'
-            />
-            <Button variant='outline-success'>Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className='navbar'>
+      {/* ... (brand logo and other tabs) ... */}
+      <img
+        src='../../ui-idea/brand-logo.png'
+        className='brand-logo'
+        alt='Brand Logo'
+      />
+      <ul className='nav-list'>
+        {/* Dropdown items  */}
+        <li
+          className='nav-item'
+          onMouseEnter={handleCollegesMouseEnter}
+          onMouseLeave={handleCollegesMouseLeave}>
+          <a href='#' className='nav-item'>
+            Colleges <ArrowDropDownIcon />
+          </a>
+          {showCollegesDropdown && (
+            <ul
+              className='dropdown'
+              onMouseEnter={handleCollegesMouseEnter}
+              onMouseLeave={handleCollegesMouseLeave}>
+              {collegesData.map((college, index) => (
+                <li key={index}>
+                  <a href={college.url} className='dropdown-item'>
+                    {college.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+        {/* ... (similar setup for Courses and Exams) ... */}
+        {/* For Courses */}
+        <li
+          className='nav-item'
+          onMouseEnter={handleCoursesMouseEnter}
+          onMouseLeave={handleCoursesMouseLeave}>
+          <a href='#' className='nav-item'>
+            Courses <ArrowDropDownIcon />
+          </a>
+          {showCoursesDropdown && (
+            <ul
+              className='dropdown'
+              onMouseEnter={handleCoursesMouseEnter}
+              onMouseLeave={handleCoursesMouseLeave}>
+              {coursesData.map((course, index) => (
+                <li key={index}>
+                  <a href={course.url} className='dropdown-item'>
+                    {course.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+        {/* For Exams */}
+        <li
+          className='nav-item'
+          onMouseEnter={handleExamsMouseEnter}
+          onMouseLeave={handleExamsMouseLeave}>
+          <a href='#' className='nav-item'>
+            Exams <ArrowDropDownIcon />
+          </a>
+          {showExamsDropdown && (
+            <ul
+              className='dropdown'
+              onMouseEnter={handleExamsMouseEnter}
+              onMouseLeave={handleExamsMouseLeave}>
+              {examsData.map((exam, index) => (
+                <li key={index}>
+                  <a href={exam.url} className='dropdown-item'>
+                    {exam.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+        {/* Static links */}
+        <li className='nav-item'>
+          <a href='#' className='nav-item'>
+            Study Abroad
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='#' className='nav-item'>
+            Get Counselling
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='#' className='nav-item'>
+            Latest News
+          </a>
+        </li>
+      </ul>
+      {/* ... (search form) ... */}{" "}
+      <form className='search-form'>
+        <input type='search' placeholder='Search' className='search-input' />{" "}
+        <button type='button' className='search-btn'>
+          Search{" "}
+        </button>{" "}
+      </form>
+    </div>
   );
 }
 
