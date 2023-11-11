57ea37f8f41416bf585e2e5545721ddc692e77bb
alpha951 <20uec068@lnmiit.ac.in>
2023-11-11 12:24:33 +0530

    ADD Stats components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import NavbarComponent from "./components/navbar/Navbar";
 import Body from "./components/body/Body";
 import Announcement from "./components/announcement/Announce";
+import Stats from "./components/stats/Stats";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavbarComponent />
       <Body />
       <Announcement />
+      <Stats />
     </>
   );
 }
---
+++ src/components/stats/Stats.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import CountUp from "react-countup";
+import "./Stats.css";
+
+const Stats = () => {
+  const [statsData, setStatsData] = useState([
+    { label: "Applications Registered", start: 0, end: 10000 },
+    { label: "Admissions Completed", start: 0, end: 1000 },
+    { label: "Students Counselled", start: 0, end: 100000 },
+    // Add more stats as needed
+  ]);
+
+  useEffect(() => {
+    // Simulate fetching data from a backend or API
+    // In a real scenario, replace this with your data fetching logic
+    // For simplicity, we're using setTimeout to simulate data loading
+    const fetchData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setStatsData((prevData) =>
+        prevData.map((stat) => ({
+          ...stat,
+          start: stat.end - 1000, // Start from 1000 less for animation effect
+        }))
+      );
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className='container-stats'>
+      {statsData.map((stat, index) => (
+        <div key={index} className='stat-item'>
+          <div className='stat-label'>{stat.label}</div>
+          <div className='stat-count'>
+            <CountUp
+              start={stat.start}
+              end={stat.end}
+              duration={2}
+              separator=','
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Stats;
